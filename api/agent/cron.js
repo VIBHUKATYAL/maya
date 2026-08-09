0784f7578f239ba1ee94ea9f7a5c4d8c446e4a53
VIBHUKATYAL <vibhukatyal75@gmail.com>
2026-08-09 17:44:10 +0530

    fix: downgrade to native 8b models to unblock groq quotas

--- api/agent/cron.js
+++ api/agent/cron.js
@@ -375,7 +375,7 @@ module.exports = async (req, res) => {
               writeResp = await fetchWithGroqFallback(
                 writePrompt,
                 groqKeys,
-                "llama-3.3-70b-versatile",
+                "llama-3.1-8b-instant",
               );
             } catch (err) {
               debugLogs.push(
